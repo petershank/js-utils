7b14d40446822603cb27c9e345ca80f10a7b4c05
Peter Shank <peter@shank.com>
2018-03-28 20:19:38 -0700

    changes from Atom?

--- primes.js
+++ primes.js
@@ -1,17 +1,18 @@
+// a collection of javascript helper functions
   function getPrimes(n) {
     var max = n;
     var integers = [];
     var primes = [];
     var i, j;
-  
-    
+
+
     for (i = 2; i < max + 1; i++) {
       integers[i] = true;
     }
-    
+
     integers[0] = false;
     integers[1] = false;
-    
+
     i = 2;
     while (i <= Math.floor(Math.sqrt(max)) ) {
       if (integers[i]) {
@@ -21,7 +22,7 @@
       }
       i++ ;
     }
-  
+
     for (i = 0; i < integers.length; i++) {
       if (integers[i]) {
         primes.push(i);
@@ -29,19 +30,19 @@
     }
     return primes;
   }
-  
+
   function getPiFunction(n) {
     if (typeof n === "string") {
       throw new TypeError("Expected an integer greater than one, but got a string");
     }
    return getPrimes(n).length;
   }
-  
+
   function isPrime(n) {
     validateType(n, 'number');
     var primes = getPrimes(Math.floor(Math.sqrt(n)));
     var returnValue = true;
-  
+
     for (var i = 0; i < primes.length; i++) {
       if (n  % primes[i] === 0) {
         returnValue = false;
@@ -50,15 +51,15 @@
     }
     return returnValue;
   }
-  
+
   function isEmirp(n) {
     return isPrime(n) && isPrime(getMirror(n)) ;
   }
-  
+
   //todo: fix bug here.  numbers are returned as strings
   function getMirror(arg) {
     var returnValue;
-    
+
     switch (typeof arg) {
       case 'string':
         returnValue = arg.split().reverse().join();
