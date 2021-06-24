2b3a265b77ee2950784a91648914bca74c9d2956
Peter Shank <peter@shank.com>
2021-06-23 21:38:21 -0700

    refactor primes.js

--- primes/primes.js
+++ primes/primes.js
@@ -1,98 +1,91 @@
-// a collection of javascript helper functions
-var check = require('check-types')
+"use strict";
 
-function getPrimes (n) {
-  var max = n
-  var integers = []
-  var primes = []
-  var i, j
-
-  if (check.not.integer(n)) {
-    throw new TypeError('Expected an integer')
-  }
-
-  if (check.not.greaterOrEqual(n, 2)) {
-    throw new TypeError('Expected an integer 2 or greater')
-  }
+function primes(n) {
+  var max = n;
+  var integers = [];
+  var primes = [];
+  var i, j;
 
   for (i = 2; i < max + 1; i++) {
-    integers[i] = true
+    integers[i] = true;
   }
 
-  integers[0] = false
-  integers[1] = false
+  integers[0] = false;
+  integers[1] = false;
 
-  i = 2
+  i = 2;
   while (i <= Math.floor(Math.sqrt(max))) {
     if (integers[i]) {
       for (j = i * i; j <= max; j = j + i) {
-        integers[j] = false
+        integers[j] = false;
       }
     }
-    i++
+    i++;
   }
 
   for (i = 0; i < integers.length; i++) {
     if (integers[i]) {
-      primes.push(i)
+      primes.push(i);
     }
   }
-  return primes
+  return primes;
 }
 
-function getPiFunction (n) {
+function getPiFunction(n) {
   if (check.not.integer(n)) {
-    throw new TypeError('Expected an integer')
+    throw new TypeError("Expected an integer");
   }
-  return getPrimes(n).length
+  return primes(n).length;
 }
 
-function isPrime (n) {
-  var primes = getPrimes(Math.floor(Math.sqrt(n)))
-  var returnValue = true
+function isPrime(n) {
+  var primes = primes(Math.floor(Math.sqrt(n)));
+  var returnValue = true;
 
   if (check.not.integer(n)) {
-    throw new TypeError('Expected an integer')
+    throw new TypeError("Expected an integer");
   }
 
   if (check.not.greaterOrEqual(n, 2)) {
-    throw new TypeError('Expected an integer 2 or greater')
+    throw new TypeError("Expected an integer 2 or greater");
   }
 
   for (var i = 0; i < primes.length; i++) {
     if (n % primes[i] === 0) {
-      returnValue = false
-      break
+      returnValue = false;
+      break;
     }
   }
-  return returnValue
+  return returnValue;
 }
 
-function isEmirp (n) {
-  return isPrime(n) && isPrime(getMirror(n))
+function isEmirp(n) {
+  return isPrime(n) && isPrime(getMirror(n));
 }
 
 // todo: fix bug here.  numbers are returned as strings
-function getMirror (arg) {
-  var returnValue
+function getMirror(arg) {
+  var returnValue;
 
   switch (typeof arg) {
-    case 'string':
-      returnValue = arg.split().reverse().join()
-      break
-    case 'number':
-      returnValue = parseInt(arg.toString(10).split().reverse().join(), 10)
-      break
+    case "string":
+      returnValue = arg.split().reverse().join();
+      break;
+    case "number":
+      returnValue = parseInt(arg.toString(10).split().reverse().join(), 10);
+      break;
     default:
-      console.log('foo')
+      console.log("foo");
   }
-  return returnValue
+  return returnValue;
 }
 
-module.exports.isEmirp = isEmirp
-module.exports.getPrimes = getPrimes
-module.exports.getMirror = getMirror
-module.exports.getPiFunction = getPiFunction
-module.exports.isPrime = isPrime
+module.exports.isEmirp = isEmirp;
+module.exports.getPrimes = primes;
+module.exports.getMirror = getMirror;
+module.exports.getPiFunction = getPiFunction;
+module.exports.isPrime = isPrime;
 
-// console.log(getPrimes(2))
+//console.log(primes(11));
+let count = 137;
+console.log(parseInt(count.toString().split("").reverse().join("")) + 1);
